c8e3c7598d30d5d40dd3266cee15ba119939d78f
Bro sal <youlihau36@gmail.com>
2026-09-24 13:30:27 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -24,8 +24,7 @@ export const FIREBASE_CONFIG = {
   storageBucket: "negotiation-46372.firebasestorage.app",
   messagingSenderId: "1013471011085",
   appId: "1:1013471011085:web:bc79da025c1af961c51044",
-measurementId: "G-LHHDJHFWR4",
-   Firebase-key: BI2P0Y6sU4srwBaKaN1ycnoo4fFgtdYJU1bTAIzb63D5u3xNzwd5K0tB3clHFpPP2Ya4IgNLgfHEUkCWNi4lz3U,
+  Firebasekey: BI2P0Y6sU4srwBaKaN1ycnoo4fFgtdYJU1bTAIzb63D5u3xNzwd5K0tB3clHFpPP2Ya4IgNLgfHEUkCWNi4lz3U,
 };
 
 // Only this Gmail address is allowed to use the admin dashboard.
